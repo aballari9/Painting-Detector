3f49660562ccb7fecbcd1c671a12540fdfd4fcb4
vivekraja07 <vivekraja07@gmail.com>
2018-11-27 15:58:28 -0500

    color index pulling changed

--- color/color.py
+++ color/color.py
@@ -8,70 +8,49 @@ import glob
 import cv2
 from scipy.misc import imread
 
-hists = np.load("histograms.npy")
-queryHists = np.load("queryHistograms.npy")
+def runColors(indexList):
 
-imageFiles = np.load("imageFiles.npy")
-queryFiles = np.load("query_images.npy")
+    hists = np.load("histograms.npy")
+    queryHists = np.load("queryHistograms.npy")
 
-# argmin
-# method = cv2.HISTCMP_BHATTACHARYYA
+    imageFiles = np.load("imageFiles.npy")
+    queryFiles = np.load("query_images.npy")
 
-#argmax
-method = cv2.HISTCMP_INTERSECT
-# method = cv2.cv2.HISTCMP_CORREL
+    # argmin
+    # method = cv2.HISTCMP_BHATTACHARYYA
 
-# initialize the results dictionary
-results = np.zeros((queryHists.shape[0],hists.shape[0]), np.float32)
+    #argmax
+    method = cv2.HISTCMP_INTERSECT
+    # method = cv2.cv2.HISTCMP_CORREL
 
-# for i in range(1):
-for i in range(queryHists.shape[0]):
-    for j in range(hists.shape[0]):
-        # compute the distance between the two histograms
-        # using the method and update the results dictionary
-        d = cv2.compareHist(queryHists[i], hists[j], method)
-        results[i][j] = d
+    # initialize the results dictionary
+    results = np.zeros((queryHists.shape[0],hists.shape[0]), np.float32)
 
-        if "parliament" in imageFiles[j]:
-            print d
+    # for i in range(1):
+    # for i in range(queryHists.shape[0]):
+    for i in range(len(indexList)):
+        for j in range(hists.shape[0]):
+            # compute the distance between the two histograms
+            # using the method and update the results dictionary
+            d = cv2.compareHist(queryHists[i], hists[j], method)
+            results[i][j] = d
+
+            if "parliament" in imageFiles[j]:
+                print d
 
 
-    plt.imshow(imread(queryFiles[i]))
-    plt.show()
+        plt.imshow(imread(queryFiles[i]))
+        plt.show()
 
-    i_max = np.argmax(results[i])
-    print results[i]
-    print results[i][i_max]
-    img = imageFiles[i_max]
-    plt.imshow(imread(img))
-    plt.show()
+        i_max = np.argmax(results[i])
+        print results[i]
+        print results[i][i_max]
+        img = imageFiles[i_max]
+        plt.imshow(imread(img))
+        plt.show()
 
-    print "next"
+        print "next"
 
-print results.shape
+    print results.shape
 
-np.save("results.npy", results)
-
-# sort the results
-# results = sorted([(v, k) for (k, v) in results.items()], reverse = True)
-
-# # show the query image
-# fig = plt.figure("Query")
-# ax = fig.add_subplot(1, 1, 1)
-# ax.imshow(images["monaLisa_museum.jpg"])
-# plt.axis("off")
-
-# # initialize the results figure
-# fig = plt.figure("Results: %s" % (methodName))
-# fig.suptitle(methodName, fontsize = 20)
-
-# # loop over the results
-# for (i, (v, k)) in enumerate(results):
-#     # show the result
-#     ax = fig.add_subplot(1, len(images), i + 1)
-#     ax.set_title("%s: %.2f" % (k, v))
-#     plt.imshow(images[k])
-#     plt.axis("off")
-
-# # show the OpenCV methods
-# plt.show()
+    np.save("results.npy", results)
